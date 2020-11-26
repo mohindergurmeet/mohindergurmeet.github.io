fafa453589a43ab74c3e04c31e7c11342f07d094
mohindergurmeet <75078267+mohindergurmeet@users.noreply.github.com>
2020-11-27 00:07:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const dynamicTextSpan = document.getElementById("dynamic-text");
+const text = document.querySelector(".type");
+const cursor = document.querySelector(".cursor");
+
+const textArray = ["COUPLE", "PARENTS"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 1500;
+var textArrayIndex = 0;
+var charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        cursor.classList.remove("blink");
+        dynamicTextSpan.textContent += textArray[textArrayIndex].charAt(
+            charIndex
+        );
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        cursor.classList.add("blink");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        cursor.classList.remove("blink");
+        dynamicTextSpan.textContent = textArray[textArrayIndex].substring(
+            0,
+            charIndex - 1
+        );
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        cursor.classList.add("blink");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, newTextDelay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(type, newTextDelay + 250);
+});
